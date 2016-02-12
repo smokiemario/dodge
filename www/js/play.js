d3942861efaf0eaa2f3fee5f89d6a1187145ca39
Tyler Hebner <smokiemario@github.com>
2016-02-11 19:03:14 -0500

    save

---
+++ www/js/play.js
@@ -0,0 +1,26 @@
+
+
+var DogeDodge = {};
+
+DogeDodge.Play = function () {};
+
+DogeDodge.Play.prototype = {
+
+  init: function () {
+    console.log("%c~~~ Booting the DogeDodge ~~~\n Compliments of Skilstak", "color:#fdf6e3; background:#073642"); 
+  },
+
+  preload: function () {
+    this.load.spritesheet('background','assets/background.png',320,568);
+
+  },
+
+  create: function () {
+    this.background = this.add.tileSprite(0,0,320,568,"background");
+    this.background.autoScroll(-50,50);
+    this.background.scale.set(1);
+  },
+
+  update: function () {
+  }
+}
