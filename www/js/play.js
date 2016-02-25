256f8a5408f547207a2c55192757362b0882189d
Tyler Hebner <smokiemario@github.com>
2016-02-25 18:45:22 -0500

    save

--- www/js/play.js
+++ www/js/play.js
@@ -11,16 +11,35 @@ DogeDodge.Play.prototype = {
   },
 
   preload: function () {
-    this.load.spritesheet('background','assets/background.png',320,568);
+    console.log("preloading background")
+    this.load.image('background','assets/background.png');
+    console.log("preloading dodger(player)")
+    this.load.spritesheet('dodger','assets/player.png',64,64,3)
 
   },
 
   create: function () {
+    console.log("creating background");
     this.background = this.add.tileSprite(0,0,320,568,"background");
     this.background.autoScroll(-50,50);
     this.background.scale.set(1);
+    console.log("creating dodger");
+    this.dodger = this.add.sprite(160,510,'dodger');
+    this.dodger.anchor.set(0.5,0.5);
+    this.dodger.animations.add('flame');
+    this.dodger.animations.play('flame',10,true);
+    this.cursors = game.input.keyboard.createCursorKeys();
+    console.log("use arrow keys to move")
   },
 
   update: function () {
+    if (this.cursors.left.isDown) {
+      this.dodger.x -= 10;
+      this.dodger.scale.set(-1);
+    }
+    if (this.cursors.right.isDown) {
+      this.dodger.x += 10;
+      this.dodger.scale.set(1);
+    }
   }
 }
