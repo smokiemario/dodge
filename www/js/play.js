735061147d961ffa82209dae07f13382d4a30a9a
Tyler Hebner <smokiemario@github.com>
2016-03-17 19:07:04 -0400

    save

--- www/js/play.js
+++ www/js/play.js
@@ -7,39 +7,73 @@ DogeDodge.Play = function () {};
 DogeDodge.Play.prototype = {
 
   init: function () {
+    this.speed = 12;
     console.log("%c~~~ Booting the DogeDodge ~~~\n Compliments of Skilstak", "color:#0000FF; background:#900009"); 
+    this.a = 1
   },
 
   preload: function () {
+    
     console.log("preloading background")
     this.load.image('background','assets/background.png');
     console.log("preloading dodger(player)")
     this.load.spritesheet('dodger','assets/player.png',64,64,3)
-
+    console.log("preloading obstacle")
+    this.load.spritesheet('dodge','assets/dodge.png',64,64,1)
   },
 
   create: function () {
-    console.log("creating background");
+    console.log("adding physics...")
+  
+
+    console.log("creating background...");
     this.background = this.add.tileSprite(0,0,320,568,"background");
     this.background.autoScroll(-50,50);
     this.background.scale.set(1);
-    console.log("creating dodger");
+    
+    console.log("creating dodger...");
     this.dodger = this.add.sprite(160,510,'dodger');
     this.dodger.anchor.set(0.5,0.5);
     this.dodger.animations.add('flame');
     this.dodger.animations.play('flame',10,true);
+    game.physics.arcade.enable(this.dodger)
+    this.dodger.body.collideWorldBounds = true;
     this.cursors = game.input.keyboard.createCursorKeys();
-    console.log("use arrow keys to move")
+
+    console.log("creating obsticle...")
+    this.dodge = this.add.sprite(160,510,'dodge');
+    this.dodge.anchor.set(0.5,0.5)
+    console.log("Success! : use arrow keys to move")
+    game.physics.arcade.enable(this.dodge)
+    this.dodge.body.gravity.y = 500;
+    this.dodger.body.bounce.setTo(0.5,0.5)
+    this.dodger.body.drag.setTo(600)
   },
 
   update: function () {
+    this.dodge.y += this.speed
     if (this.cursors.left.isDown) {
-      this.dodger.x -= 10;
-      this.dodger.scale.set(-1);
+      this.dodger.body.gravity.x = -1200;
     }
     if (this.cursors.right.isDown) {
-      this.dodger.x += 10;
-      this.dodger.scale.set(1);
+      this.dodger.body.gravity.x = 1200;
+    }
+    if (this.dodge.y >568) {
+      this.dodge.y = 10;
+
+      
+      
+      
+      
+    
+      
+      
+    
+      
+      this.dodge.x = game.rnd.integerInRange(0,320)
+      this.dodge.body.velocity.y = 1
+      
+      
     }
   }
 }
