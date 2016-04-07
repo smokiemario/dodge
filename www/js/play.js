4759db40a711870f7a3f67b7c0569c48aad4d2d8
Tyler Hebner <smokiemario610@gmail.com>
2016-04-07 18:36:20 -0400

    Update play.js

--- www/js/play.js
+++ www/js/play.js
@@ -69,7 +69,7 @@ DogeDodge.Play.prototype = {
       this.dodge.body.velocity.y = 1
       
    
-    },
+    }
   handleCollision: function() {
     console.log("OUCH!");
     game.state.start('play')
